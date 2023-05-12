e58a3651f75e453e7976aa19da7f4b2463af424a
Vilyak <dimvitebsk@gmail.com>
2023-05-12 21:26:12 +0300

    change node path

--- task-manager-service/src/utils/common.ts
+++ task-manager-service/src/utils/common.ts
@@ -4,7 +4,7 @@ import {JobDefinitions} from "../modules/job/jobDefinitions";
 export const getCommandLine = (jobId: number, params: Record<string, string>) => {
     const job = JobDefinitions.find(job => job.jobId === jobId);
     if (job) {
-        return `/root/.nvm/versions/node/v16.19.0/bin/node ${Scripts[job.activity]} ${Params[job.activity].map(v => `${v}="${params[v]}"`).join()}`
+        return `/usr/local/nodejs/bin/node ${Scripts[job.activity]} ${Params[job.activity].map(v => `${v}="${params[v]}"`).join()}`
     }
     throw 'Невозможной найти задачу!'
 }
