75b27cb8d7fa3681bb577cd5f571494987b9631b
Vilyak <dimvitebsk@gmail.com>
2023-05-13 12:24:49 +0300

    eqwe

--- task-manager-client/prod.js
+++ task-manager-client/prod.js
@@ -3,6 +3,7 @@ const app = express();
 app.use(express.static('build'));
 const path = require('path');
 app.get('*', (req, res) => {
+    console.log("GET");
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 const PORT = process.env.PORT;
