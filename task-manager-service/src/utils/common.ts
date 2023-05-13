c61c001579a3dc4401617f9639e7fdcbc93e76f2
Vilyak <dimvitebsk@gmail.com>
2023-05-13 15:08:48 +0300

    eqwe

--- task-manager-service/src/utils/common.ts
+++ task-manager-service/src/utils/common.ts
@@ -5,7 +5,7 @@ import {isArray} from "util";
 export const getCommandLine = (jobId: number, params: Record<string, any>) => {
     const job = JobDefinitions.find(job => job.jobId === jobId);
     if (job) {
-        return `/usr/local/nodejs/bin/node ${Scripts[job.activity]} ${Params[job.activity].map(v => `${v}="${isArray(params[v]) ? params[v].join('|') : JSON.stringify(params[v])}"`).join()}`
+        return `/usr/local/nodejs/bin/node ${Scripts[job.activity]} ${Params[job.activity].map(v => `${v}="${isArray(params[v]) ? params[v].join('|') : params[v]}"`).join()}`
     }
     throw 'Невозможной найти задачу!'
 }
